d8c246b86a56614e645e9063246cd5fcb3826f23
Ale Raf <raffinalexis@gmail.com>
2023-01-18 15:41:46 +0100

    login update

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,18 +1,32 @@
-const form = document.querySelector('#form');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const one = new FormData(form);
-    const two = new URLSearchParams(one);
-    fetch("http://localhost:5678/api/users/login", {
-        method : 'POST',
-        body : one,
-        headers : {
-                'Content-Type': 'application/json'
+function login() {
+    const form = document.querySelector('#form');
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let user = {
+        email : e.target.querySelector('#email').value,
+        password : e.target.querySelector('#password').value,
+        };
+        fetch("http://localhost:5678/api/users/login", {
+            method : 'POST',
+            headers : {
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/json'
+                    },
+            body : JSON.stringify(user)
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    alert("Les informations renseignées sont incorrectes");
                 }
-    })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => alert('error'))
-});
+            })
+            .then(value => {
+                window.localStorage.setItem("token", value.token);
+                window.location.href = "index.html";
+            })
+            .catch(err => console.log(err))
+    });
+};
 
+login();
